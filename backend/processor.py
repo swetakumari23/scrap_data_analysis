0241e5a240ac5231ef081f6f4e2d5126be4cd9be
sweta23092001 <sweta.kumari@aroha.co.in>
2026-04-21 17:27:28 +0530

    add workflow edit path

--- backend/processor.py
+++ backend/processor.py
@@ -8,7 +8,7 @@ def process_data():
 
     base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) 
 
-    file_path = os.path.join(base_dir, "data", "sample_data.csv") 
+    file_path = os.path.join(base_dir, "data", "sample.csv") 
 
  
 
